e665a837eb8a1f9b2df96fd32660ee84b73377ad
LANCER477 <maksimuntyan@gmail.com>
2026-03-16 00:22:30 +0200

    Update DataAccessor.cs
    ffsdfsdf

--- Users/Dal/DataAccessor.cs
+++ Users/Dal/DataAccessor.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,6 +12,7 @@ namespace SharpKnP321.Users.Dal
     internal class DataAccessor
     {
         private SqlConnection connection;
+        
         public DataAccessor()
         {
             String settingsFilename = "appsettings.json";
@@ -29,27 +30,23 @@ namespace SharpKnP321.Users.Dal
             {
                 var csSection = settings.GetProperty("ConnectionStrings");
                 userDb = csSection.GetProperty("UserDB").GetString()!;
-
             }
             catch (Exception ex)
             {
                 throw new Exception($"Помилка визначення конфігурації: {ex.Message}");
-
             }
+            
             connection = new(userDb);
+            
             try
             {
                 connection.Open();
-
             }
             catch (Exception ex)
             {
                 throw new Exception($"Помилка визначення конфігурації: {ex.Message}");
-
             }
-
         }
-
 
         public void SignUp(UserData userData)
         {
@@ -64,7 +61,43 @@ namespace SharpKnP321.Users.Dal
             }
         }
 
+        // --- НАЧАЛО ДЗ: Проверка активного токена при аутентификации ---
+        // Д.З. При автентифікації здійснювати перевірку чи є в користувача 
+        // активний токен. У такому разі подовжувати його дію, замість 
+        // генерування нового. За відсутності - генерувати новий.
+        public string CreateOrUpdateToken(Guid accessId)
+        {
+            var existingTokenId = connection.QueryFirstOrDefault<Guid?>(
+                "SELECT TokenId FROM AccessToken WHERE AccessId = @accessId AND TokenExp > CURRENT_TIMESTAMP",
+                new { accessId }
+            );
 
+            if (existingTokenId != null && existingTokenId != Guid.Empty)
+            {
+                DateTime newExpiration = DateTime.Now.AddHours(1);
+                
+                connection.Execute(
+                    "UPDATE AccessToken SET TokenExp = @newExp WHERE TokenId = @tokenId",
+                    new { newExp = newExpiration, tokenId = existingTokenId }
+                );
+                
+                return existingTokenId.ToString();
+            }
+            else
+            {
+                Guid newTokenId = Guid.NewGuid();
+                DateTime now = DateTime.Now;
+                DateTime expiration = now.AddHours(1);
+
+                connection.Execute(
+                    "INSERT INTO AccessToken (TokenId, AccessId, TokenIat, TokenExp) VALUES (@tokenId, @accessId, @iat, @exp)",
+                    new { tokenId = newTokenId, accessId = accessId, iat = now, exp = expiration }
+                );
+                
+                return newTokenId.ToString();
+            }
+        }
+       
 
         public void Install(bool isHard = false)
         {
@@ -106,6 +139,4 @@ namespace SharpKnP321.Users.Dal
     )");
         }
     }
-
-
 }
