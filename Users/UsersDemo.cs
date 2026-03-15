52ae5891ed14f0e41d843ff392229094ade474c8
LANCER477 <maksimuntyan@gmail.com>
2026-03-16 00:18:41 +0200

    gdfgdf
    Added password validation logic during user registration.

--- Users/UsersDemo.cs
+++ Users/UsersDemo.cs
@@ -1,7 +1,8 @@
-﻿using SharpKnP321.Users.Dal;
+using SharpKnP321.Users.Dal;
 using SharpKnP321.Users.Dal.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -32,7 +33,6 @@ namespace SharpKnP321.Users
             {
                 _accessor = new();
             }
-
             catch(Exception ex)
             {
                 Console.WriteLine(ex.Message);
@@ -69,6 +69,7 @@ namespace SharpKnP321.Users
             UserData userData = new();
             bool isEntryCorrect;
             Console.WriteLine("Регистрация");
+            
             do
             {
                 Console.WriteLine("Полное имя");
@@ -80,26 +81,76 @@ namespace SharpKnP321.Users
                     Console.WriteLine("Слишком короткий");
                 }
             } while (!isEntryCorrect);
+            
             do
             {
                 Console.WriteLine("email");
                 userData.UserEmail = Console.ReadLine()!.Trim()!;
                 if (userData.UserEmail == String.Empty) return;
-                isEntryCorrect = Regex.IsMatch(userData.UserEmail,@"^\w + ([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$");
-                if (isEntryCorrect)
+                isEntryCorrect = Regex.IsMatch(userData.UserEmail,@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$");
+                if (!isEntryCorrect)
                 {
                     Console.WriteLine("Не відповідає формату");
                 }
             } while (!isEntryCorrect);
+
+            // ДЗ: Валідація паролю
+            string validPassword = ReadAndValidatePassword();
+            Console.WriteLine("Пароль успішно прийнято!"); 
+            
         }
+
+        // ДЗ: Валідація паролю 
+        private string ReadAndValidatePassword()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введіть пароль для реєстрації: ");
+                string password = Console.ReadLine() ?? "";
+
+                List<string> errors = new List<string>();
+
+                if (password.Length < 6)
+                {
+                    errors.Add("- довжина не менша 6 символів");
+                }
+                if (!password.Any(char.IsDigit))
+                {
+                    errors.Add("- містить щонайменше одну цифру");
+                }
+                if (!password.Any(c => !char.IsLetterOrDigit(c)))
+                {
+                    errors.Add("- містить щонайменше один спецсимвол (не літера, не цифра)");
+                }
+                if (!password.Any(char.IsLower))
+                {
+                    errors.Add("- містить щонайменше одну літеру нижнього реєстру (малу)");
+                }
+                if (!password.Any(char.IsUpper))
+                {
+                    errors.Add("- містить щонайменше одну літеру верхнього реєстру (велику)");
+                }
+
+                if (errors.Count == 0)
+                {
+                    return password; 
+                }
+
+                Console.WriteLine("\nПароль не відповідає вимогам безпеки. Порушені критерії:");
+                foreach (string error in errors)
+                {
+                    Console.WriteLine(error);
+                }
+                Console.WriteLine("Будь ласка, спробуйте ще раз.\n");
+            }
+        }
+       
     }
 }
 /* Робота з користувачами: реєстрація, автентифікація, авторизація
- * 
- * UserData        UserAccess        AccessTokens
- *  UserId          AccessId          TokenId
- *  UserName        AccessLogin       AccessId
- *  UserEmail       AccessSalt        TokenIat
- *  UserDelAt       AccessDk          TokenExp 
- * 
- */
+ * * UserData        UserAccess        AccessTokens
+ * UserId          AccessId          TokenId
+ * UserName        AccessLogin       AccessId
+ * UserEmail       AccessSalt        TokenIat
+ * UserDelAt       AccessDk          TokenExp 
+ * */
